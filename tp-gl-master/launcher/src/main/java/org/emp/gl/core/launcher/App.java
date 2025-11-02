f83ac1a099f74e5962ffda5b3eed74a96c9b08b3
Anes <aneshawew001@keemail.me>
2025-11-02 14:54:17 +0000

    validation de la tache d

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -3,7 +3,7 @@ package org.emp.gl.core.launcher;
 import org.emp.gl.clients.Horloge;
 import org.emp.gl.timer.service.TimerService;
 import org.emp.gl.timer.service.impl.DummyTimeServiceImpl;
- 
+import org.emp.gl.clients.CompteARebours;
  /**
  * Hello world!
  *
@@ -20,9 +20,19 @@ public class App {
         TimerService timerService = new DummyTimeServiceImpl();
 
         // Injecter le service dans les horloges (elles s'inscrivent automatiquement)
-        new Horloge("Num 1", timerService);
-        new Horloge("Num 2", timerService);
-        new Horloge("Num 3", timerService);
+        //new Horloge("Num 1", timerService);
+        //new Horloge("Num 2", timerService);
+        //new Horloge("Num 3", timerService);
+
+        // Test d’un compte à rebours de 5 secondes
+        new CompteARebours("Rebours 1", 5, timerService);
+
+        // Test de plusieurs comptes à rebours aléatoires
+        for (int i = 1; i <= 5; i++) {
+            int randomValue = 10 + (int)(Math.random() * 6); // entre 10 et 15
+            new CompteARebours("Rebours " + i, randomValue, timerService);
+        }
+
 
         // laisser l'application tourner (sinon JVM peut se terminer si tout est daemon)
         try {
